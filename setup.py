41991468ed5991dde4204decf05095db0d829b01
Philip <p208p2002@gmail.com>
2020-09-05 11:39:51 +0800

    no message

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 import setuptools
 
-
-with open("README.md", "r",encoding="utf-8") as fh:
-    long_description = fh.read()
-    
-# with open('requirements.txt','r',encoding = 'utf-8') as f:
-    # requirements = f.read().split("\n")
-
 setuptools.setup(
     name="docker_for_ai_dev_cli", # 
     version='0.0.1',
     author="Philip Huang",
     author_email="p208p2002@gmail.com",
     description="docker-for-ai-dev-cli",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/p208p2002/docker-for-ai-dev-cli",
     packages=setuptools.find_packages(),
     classifiers=[
